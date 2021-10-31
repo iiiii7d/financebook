249bf62c1c6b237c760792c68f396465da9648bb
7d <i.second.7d@gmail.com>
2021-10-31 20:40:59 +0800

    records and notes pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,26 @@
 var menuExpanded = false;
 function toggleMenu(b) {
     if (!b) {
-        document.getElementsByTagName("nav")[0].classList.remove("selected");
+        $("nav")[0].classList.remove("selected");
     } else {
-        document.getElementsByTagName("nav")[0].classList.add("selected");
+        $("nav")[0].classList.add("selected");
     }
+}
+
+$('body')[0].addEventListener('onload', () => {
+    loadPage("players")
+})
+
+const Pagetitles = {
+    'players': 'Players',
+    'records': 'Records',
+    'notes': 'Notes'
+}
+
+function loadPage(page) {
+    $("#pagetitle")[0].innerHTML = Pagetitles[page];
+    $("main")[0].setAttribute("class", "page-"+page);
+    $.get(`pages/${page}.html`, data => {
+        $('main').html(data);
+    });
 }
