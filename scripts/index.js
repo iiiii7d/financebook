26f4166b60902fc7d1d36867f5b5f7d6578584d4
7d <i.second.7d@gmail.com>
2021-11-01 19:31:51 +0800

    loading screen, fixed position sidebar and data loading for players

--- scripts/index.js
+++ scripts/index.js
@@ -7,20 +7,53 @@ function toggleMenu(b) {
     }
 }
 
-$('body')[0].addEventListener('onload', () => {
-    loadPage("players")
-})
-
-const Pagetitles = {
+const PageTitles = {
     'players': 'Players',
     'records': 'Records',
     'notes': 'Notes'
 }
+const PageFunctions = {
+    'players': PlayersFunctions
+}
+
+function getData() {
+    return JSON.parse(LZString.decompress(localStorage['server']));
+}
 
 function loadPage(page) {
-    $("#pagetitle")[0].innerHTML = Pagetitles[page];
+    $("#pagetitle")[0].innerHTML = PageTitles[page];
     $("main")[0].setAttribute("class", "page-"+page);
+    $("main").html("<b>Loading...</b>")
     $.get(`pages/${page}.html`, data => {
-        $('main').html(data);
+        $('main').html(PageFunctions[page].init(data));
+        $('nav')[0].style.height = $('html').height();
     });
+}
+
+if (!('server' in localStorage)) {
+    localStorage['server'] = LZString.compress(JSON.stringify({
+        players: {
+            "foobar": 100,
+            "foobar2": -100
+        },
+        records: [
+            {
+                name: "foobar",
+                money: 100,
+                date: new Date()
+            },
+            {
+                name: "foobar2",
+                money: -100,
+                date: new Date()
+            }
+        ],
+        notes: [
+            {
+                date: new Date(),
+                title: "title",
+                content: "lorem ipsum dolor"
+            }
+        ]
+    }))
 }
---
+++ scripts/players.js
@@ -0,0 +1,16 @@
+const PlayersFunctions = {
+  init: (data) => {
+    let ele = document.createElement("main");
+    ele.innerHTML = data;
+    let template = ele.querySelector("#template");
+    Object.entries(getData().players).forEach(([player, amt]) => {
+      let box = document.createElement("div");
+      box.innerHTML = template.innerHTML;
+      box.querySelector(".name").innerHTML = player;
+      box.querySelector(".money").innerHTML = Math.abs(amt);
+      box.querySelector(".dir").innerHTML = "owed by "+(amt<0 ? "them" : "me");
+      ele.appendChild(box);
+    });
+    return ele.innerHTML;
+  }
+}
