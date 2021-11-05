bc98811d6363ef25099b3d6c3b82a768992b846a
7d <i.second.7d@gmail.com>
2021-11-05 23:28:46 +0800

    added type switcher

--- scripts/add.js
+++ scripts/add.js
@@ -1,4 +1,11 @@
 const AddFunctions = {
   init: (data) => {
     return data
-}};
+  },
+  toggleMode: (mode) => {
+    document.querySelectorAll("span.type-selector").forEach(btn => {
+      if (btn.innerHTML == mode) btn.classList.add("selected");
+      else btn.classList.remove("selected");
+    })
+  }
+};
